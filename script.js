030dfadca3a3829ad5ae590f102a365c52a5cb7b
AnaLauraFFerraz <analaurafferraz@gmail.com>
2022-11-04 20:17:13 -0300

    Tela criação de perguntas

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const containerQuizzesOutros = document.querySelector(".todos-os-quizzes-container")
+const containerQuizzesOutros = document.querySelector(".criarQuiz");
+const criarQuizzPerguntas = document.querySelector(".criarQuiz-perguntas");
+const criarQuizzNiveis = document.querySelector(".criarQuiz-niveis");
+const criarQuizzSucesso = document.querySelector(".criarQuiz-sucesso");
+
 let listaQuizzesOutros ="";
 
 
@@ -18,4 +22,68 @@ function getQuizzes(){
     })
 }
 
-getQuizzes()
+getQuizzes();
+
+// ------ Criação Quizz ------
+
+// Tela 1 - Informações básicas
+
+// Tela 2 - Perguntas
+
+function criarPerguntas(){
+    
+    // Tela anterior .classList.add("escondido");
+    criarQuizzPerguntas.classList.remove("escondido");
+
+    let i=0;
+    const numPerguntas = 3; // Número de perguntas selecionadas
+    let perguntaInfo =`<h3>Crie suas perguntas</h3>`;
+
+    for (let i = 0; i < numPerguntas; i++) {
+        perguntaInfo += `
+            <div class="criar-pergunta">
+                <button type="button" class="button-editar" onclick="colapsar(this)">
+                    <h4>Pergunta ${[i]}</h4>
+                    <ion-icon class="icon-editar" name="create-outline"></ion-icon>
+                </button>    
+   
+                <div class="criar-pergunta-container escondido">
+                    <input type="text" placeholder="Texto da pergunta">
+                    <input type="text" placeholder="Cor de fundo da pergunta">
+
+                    <h4>Resposta correta</h4>
+                    <input type="text" placeholder="Resposta correta">
+                    <input type="url" placeholder="URL da imagem">
+
+                    <h4>Respostas incorretas</h4>
+                    <input type="text" placeholder="Resposta incorreta 1">
+                    <input type="url" placeholder="URL da imagem 1">
+                    <input type="text" placeholder="Resposta incorreta 2">
+                    <input type="url" placeholder="URL da imagem 2">
+                    <input type="text" placeholder="Resposta incorreta 3">
+                    <input type="url" placeholder="URL da imagem 3">
+                </div>
+            </div>`
+    }
+
+    criarQuizzPerguntas.innerHTML += perguntaInfo;
+    criarQuizzPerguntas.innerHTML += `<button onclick="criarNiveis()">Prosseguir para criar níveis</button>`;
+
+    const colapsarPerguntas = document.querySelectorAll(".criar-pergunta-container");
+    colapsarPerguntas[0].classList.remove("escondido");
+}
+
+function colapsar(element){ // Para minimizar seção na tela de criação das perguntas
+    const buttonEditar = document.querySelectorAll(".button-editar");
+
+    for (let i = 0; i < numPerguntas; i++) {
+        buttonEditar[i].nextElementSibling.classList.add("escondido");
+    }
+    element.nextElementSibling.classList.remove("escondido")
+}
+
+// Tela 3 - Níveis
+
+function criarNiveis() {}
+
+// Tela 4 - Sucesso
