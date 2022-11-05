32a7942a704f2cdcf7d4eb8f4e7f38573c8fa5ca
heliogustavo <jacomehelioiq@gmail.com>
2022-11-05 12:16:30 -0400

    Tela 'Comece pelo começo' - Desktop

--- script.js
+++ script.js
@@ -19,3 +19,43 @@ function getQuizzes(){
 }
 
 getQuizzes()
+
+// Tela 'Comece pelo começo';
+
+const listaDeQuizz = [];
+
+function recebendoConfigDoQuizz() {
+    const tituloInput = document.getElementById("titulo");
+    const urlInput = document.getElementById("url");
+    const perguntasInput = document.getElementById("perguntas");
+    const niveisInput = document.getElementById("niveis");
+
+    const titulo = tituloInput.value;
+    const url = urlInput.value;
+    const perguntas = perguntasInput.value;
+    const niveis = niveisInput.value;
+//objeto - Quizz Criado:
+    const objQuizzCriado = {tituloDoQuizz: titulo, 
+                            urlDoQuizz: url, 
+                            qntdPerguntas: perguntas, 
+                            qntdNiveis: niveis};
+    listaDeQuizz.push(objQuizzCriado);
+
+    if(titulo.length < 20){
+        alert("O Título do Quizz precisa ter pelo menos 20 caracteres :)");
+      }
+    if(url === undefined){
+        alert("Você precisa inserir um Link de imagem válido :( ");
+      }
+    if(perguntas < 1){
+        alert("É preciso inserir pelo menos 1 pergunta ;)");
+      }
+    if(perguntas > 20){
+        alert("O limite de perguntas é 20 :/");
+      }
+    if(niveis > 3){
+        alert("A quantidade de níveis deve ser entre 1 e 3 :)");
+      }
+      console.log(url.value);
+}
+
